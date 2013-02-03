f3709c5160752ca9e881276afc8b96a0681f5cd8
Hans Klunder <hans.klunder@xs4all.nl>
2013-02-03 10:29:48 -0500

    fixed coding style issues

--- lightswitch.js
+++ lightswitch.js
@@ -1,25 +1,25 @@
-var switchCmd='/usr/local/bin/lightswitch';
+var SWITCHCMD='/usr/local/bin/lightswitch';
 
-var switchmap={
-	"device1":{ "group":"B","device":1},
-	"device2":{ "group":"B","device":2},
-	"device3":{ "group":"B","device":3},
-	"device4":{ "group":"C","device":1},
-	"device5":{ "group":"C","device":2},
-	"power":{ "group":"C","device":2}
-}
+var switchMap={
+	'device1':{ 'group':'B','device':1},
+	'device2':{ 'group':'B','device':2},
+	'device3':{ 'group':'B','device':3},
+	'device4':{ 'group':'C','device':1},
+	'device5':{ 'group':'C','device':2},
+	'power':{ 'group':'C','device':2}
+};
 
-exports.switch=function(k,v){
-	if (! switchmap[k]) return;
-	var dev=switchmap[k];
-	console.log(switchCmd,"-g",dev.group,"-n",dev.device,v);
-	var spawn = require('child_process').spawn,
-     lightswitch = spawn(switchCmd, ['-g',dev.group,'-n',dev.device,v]);
+exports.switch=function(key,value){
+	if (! switchMap[key]) return;
+	var dev=switchMap[key];
+	console.log(SWITCHCMD,'-g',dev.group,'-n',dev.device,value);
+	var spawn = require('child_process').spawn;
+    var lightSwitch = spawn(SWITCHCMD, ['-g',dev.group,'-n',dev.device,value]);
 
-	lightswitch.on('exit', function (code) {
+	lightSwitch.on('exit', function (code) {
 		if (code !== 0) {
-			console.log('lightswitch process exited with code ' + code);
+			console.log(SWITCHCMD,'exited with code',code);
 		}
 	});
-}
+};
 
--- server.js
+++ server.js
@@ -2,41 +2,41 @@
 var ls=require('./lightswitch');
 
 var state={
-    "auto":"on",
-    "device1":"off",
-    "device2":"off",
-    "device3":"off",
-    "device4":"off",
-    "device5":"off",
-	"power":{}
+    'auto':'on',
+    'device1':'off',
+    'device2':'off',
+    'device3':'off',
+    'device4':'off',
+    'device5':'off',
+	'power':{}
 };
 
-var statevals={"on":"1","off":"0"};
+var statevals={'on':'1','off':'0'};
 
-function handlePower(q,c){
-	if (! q) return;
-	if (! q.cmd) return;
-	c="ip" + c;
-	if (q.cmd == "on"){
+function handlePower(query,client){
+	if (! query) return;
+	if (! query.cmd) return;
+	client='ip' + client;
+	if (query.cmd === 'on'){
 		if (Object.keys(state.power).length === 0)
-			ls.switch('power',q.cmd);
-		state.power[c]="active";
+			ls.switch('power',query.cmd);
+		state.power[client]='active';
 	}
-	if (q.cmd == "off"){
-		delete(state.power[c]);
+	if (query.cmd === 'off'){
+		delete(state.power[client]);
 		if (Object.keys(state.power).length === 0)
-			ls.switch('power',q.cmd);
+			ls.switch('power',query.cmd);
 	}
-	if (q.cmd == "del"){
+	if (query.cmd === 'del'){
 		state.power={};
 		ls.switch('power','off');
 	}
 }
 
-function handleState(q){
-	if (! q) return;
-	Object.keys(q).forEach(function(key) {
-        var val = q[key];
+function handleState(query){
+	if (! query) return;
+	Object.keys(query).forEach(function(key) {
+        var val = query[key];
 		if (! statevals[val]) return;
         if (state[key] !== val){
 			ls.switch(key,val);
@@ -46,8 +46,8 @@ function handleState(q){
 
 }
 
-var connect = require('connect')
-  , http = require('http');
+var connect = require('connect');
+var http = require('http');
 
 
 var app = connect()
@@ -67,18 +67,18 @@ var app = connect()
     res.end('404: Page not found\n');
   });
 
-// required for c9.io compatibility
-var host = "localhost";
+
+var host = 'localhost';
 var port = 80;
-    
+
+// required for c9.io testing compatibility
 if (process.env.C9_PID)
 {
     host=process.env.IP;
     port=process.env.PORT;
 }
 else
-    console.log("Server is running at","http://"+host+":"+port);
-
+    console.log('Server is running at','http://'+host+':'+port);
 
 http.createServer(app).listen(port,host);
 
