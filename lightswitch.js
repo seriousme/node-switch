9c82aaf1947149e7a0ba68210b245503aad7e292
Hans Klunder <hans.klunder@xs4all.nl>
2017-01-28 09:30:18 +0100

    reformatted all javascript to standard format
    Reformatted to standard format ( http://standardjs.com/ ) using
    standard-format

--- lightswitch.js
+++ lightswitch.js
@@ -1,25 +1,24 @@
-var SWITCHCMD='/usr/local/bin/lightswitch';
+var SWITCHCMD = '/usr/local/bin/lightswitch'
 
-var switchMap={
-	'device1':{ 'group':'B','device':1},
-	'device2':{ 'group':'B','device':2},
-	'device3':{ 'group':'B','device':3},
-	'device4':{ 'group':'C','device':1},
-	'device5':{ 'group':'C','device':2},
-	'power':{ 'group':'C','device':2}
-};
+var switchMap = {
+  'device1': { 'group': 'B','device': 1},
+  'device2': { 'group': 'B','device': 2},
+  'device3': { 'group': 'B','device': 3},
+  'device4': { 'group': 'C','device': 1},
+  'device5': { 'group': 'C','device': 2},
+  'power': { 'group': 'C','device': 2}
+}
 
-exports.switch=function(key,value){
-	if (! switchMap[key]) return;
-	var dev=switchMap[key];
-	console.log(SWITCHCMD,'-g',dev.group,'-n',dev.device,value);
-	var spawn = require('child_process').spawn;
-    var lightSwitch = spawn(SWITCHCMD, ['-g',dev.group,'-n',dev.device,value]);
+exports.switch = function (key, value) {
+  if (! switchMap[key]) return
+  var dev = switchMap[key]
+  console.log(SWITCHCMD, '-g', dev.group, '-n', dev.device, value)
+  var spawn = require('child_process').spawn
+  var lightSwitch = spawn(SWITCHCMD, ['-g', dev.group, '-n', dev.device, value])
 
-	lightSwitch.on('exit', function (code) {
-		if (code !== 0) {
-			console.log(SWITCHCMD,'exited with code',code);
-		}
-	});
-};
-
+  lightSwitch.on('exit', function (code) {
+    if (code !== 0) {
+      console.log(SWITCHCMD, 'exited with code', code)
+    }
+  })
+}
--- server.js
+++ server.js
@@ -1,67 +1,65 @@
+var ls = require('./lightswitch')
 
-var ls=require('./lightswitch');
-
-var state={
-    'auto':'on',
-    'device1':'off',
-    'device2':'off',
-    'device3':'off',
-    'device4':'off',
-    'device5':'off',
-	'power':{}
-};
-
-var statevals={'on':'1','off':'0'};
-
-function handlePower(query,client){
-	if (! query) return;
-	if (! query.cmd) return;
-	client='ip' + client;
-	if (query.cmd === 'on'){
-		if (Object.keys(state.power).length === 0)
-			ls.switch('power',query.cmd);
-		state.power[client]='active';
-	}
-	if (query.cmd === 'off'){
-		delete(state.power[client]);
-		if (Object.keys(state.power).length === 0)
-			ls.switch('power',query.cmd);
-	}
-	if (query.cmd === 'del'){
-		state.power={};
-		ls.switch('power','off');
-	}
+var state = {
+  'auto': 'on',
+  'device1': 'off',
+  'device2': 'off',
+  'device3': 'off',
+  'device4': 'off',
+  'device5': 'off',
+  'power': {}
 }
 
-function handleState(query){
-	if (! query) return;
-	Object.keys(query).forEach(function(key) {
-    var val = query[key];
-		if (! statevals[val]) return;
-    if (state[key] !== val){
-			ls.switch(key,val);
-			state[key] = val;
-		}
-	});
+var statevals = {'on': '1','off': '0'}
 
+function handlePower (query, client) {
+  if (! query) return
+  if (! query.cmd) return
+  client = 'ip' + client
+  if (query.cmd === 'on') {
+    if (Object.keys(state.power).length === 0)
+      ls.switch('power', query.cmd)
+    state.power[client] = 'active'
+  }
+  if (query.cmd === 'off') {
+    delete (state.power[client])
+    if (Object.keys(state.power).length === 0)
+      ls.switch('power', query.cmd)
+  }
+  if (query.cmd === 'del') {
+    state.power = {}
+    ls.switch('power', 'off')
+  }
 }
 
-var port = 8080;
-var staticSite = __dirname + '/public';
-var express = require('express');
+function handleState (query) {
+  if (! query) return
+  Object.keys(query).forEach(function (key) {
+    var val = query[key]
+    if (! statevals[val]) return
+    if (state[key] !== val) {
+      ls.switch(key, val)
+      state[key] = val
+    }
+  })
+}
+
+var port = 8080
+var staticSite = __dirname + '/public'
+var express = require('express')
 var app = express()
-app.use('/cgi-bin/switch',function(req, res){
-    handleState(req.query);
-    res.end(JSON.stringify(state));
-   })
-app.use('/cgi-bin/power',function(req, res){
-    handlePower(req.query,req.connection.remoteAddress);
-    res.end(JSON.stringify(state));
-   })
-app.use('/', express.static(staticSite));
-app.use(function(req, res){
-    res.writeHead(404, {'content-type':'text/plain'});
-    res.end('404: Page not found\n');
-  });
+app.use('/cgi-bin/switch', function (req, res) {
+  handleState(req.query)
+  res.end(JSON.stringify(state))
+})
+app.use('/cgi-bin/power', function (req, res) {
+  handlePower(req.query, req.connection.remoteAddress)
+  res.end(JSON.stringify(state))
+})
+app.use('/', express.static(staticSite))
+app.use(function (req, res) {
+  res.writeHead(404, {'content-type': 'text/plain'})
+  res.end('404: Page not found\n')
+})
 
-app.listen(port, function() { console.log('listening')});
+app.listen(port, function () { console.log('listening')})
