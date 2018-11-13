6f55511abf33a34481a967b509c198e384c43f87
Hans Klunder <hans.klunder@xs4all.nl>
2018-11-13 18:23:22 +0100

    Update rules/rules.js

--- rules/rules.js
+++ rules/rules.js
@@ -70,7 +70,7 @@ function handleSunRise() {
 
 async function handleSunSet() {
   debug("sunSet");
-  await sunWait("sunset", 4620);
+  await sunWait("sunset", 3420);
   app.publish("lamp/1/auto", "on");
   await sleep(600);
   app.publish("lamp/2/auto", "on");
