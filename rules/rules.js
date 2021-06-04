3c7ecd7f83f7adcbad7a990875a99946c68f8704
Hans Klunder <hans.klunder@gmail.com>
2021-06-04 11:05:55 +0200

    adjust sunblock

--- rules/rules.js
+++ rules/rules.js
@@ -158,7 +158,7 @@ async function handleBlindsSet(req) {
       if (State.get("config/sunblock") === "on") {
         if (topic.startsWith("blinds/front") && isSunnyForecast(21)) {
           deviceSwitch(topic, "down");
-          await sleep(13);
+          await sleep(11);
           deviceSwitch(topic, "down");
         }
         if (topic.startsWith("blinds/side") && isSunnyForecast()) {
