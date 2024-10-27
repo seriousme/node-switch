667e364a5b7368653c78d5f1dcedce386eca64b7
Hans Klunder <hans.klunder@gmail.com>
2024-10-27 16:30:44 +0100

    update demo defaults

--- client/src/mqttclient.js
+++ client/src/mqttclient.js
@@ -13,10 +13,20 @@ const mqttDemo = {
 		this.publish("lamp/2/set", "off");
 		this.publish("lamp/3/set", "off");
 		this.publish("lamp/4/set", "off");
+		this.publish("power/set", "off");
 		this.publish(
 			"data/forecast/set",
-			'{"date":1541832934232,"weer":"bewolkt","tmax":13,"tmin":10,"windk":3,"windr":"Z","neerslag":29,"zon":3}',
-		);
+			JSON.stringify({
+					date: '2024-10-27T15:08:03.000Z',
+					weer: 'halfbewolkt',
+					max_temp: 16,
+					min_tem: 9,
+					windbft: 1,
+					windr: 'ZW',
+					neersl_perc_dag: 0,
+					zond_perc_dag: 47
+				  })
+	);
 		this.publish("config/auto/set", "on");
 		this.publish("config/sunblock/set", "on");
 		this.publish("config/useweather/set", "on");
